842ca1e19974ebafcc2b72dd31ec754767dc3e55
nodeman <nodeman@icloud.com>
2020-08-19 12:51:53 +0900

    Initial commit

---
+++ src/main/java/com/springexam/SpringExamApplication.java
@@ -0,0 +1,14 @@
+package com.springexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamApplication.class, args);
+		int i=0;
+	}
+
+}
