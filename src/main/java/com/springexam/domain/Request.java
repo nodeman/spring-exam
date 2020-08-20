d0fe2ceadbf1aef43b0f0b14cf33bf11feadbd73
nodeman <nodeman@icloud.com>
2020-08-20 16:39:38 +0900

    make models

--- src/main/java/com/springexam/domain/Request.java
+++ src/main/java/com/springexam/domain/Request.java
@@ -1,5 +1,58 @@
 package com.springexam.domain;
 
-public class Request {
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.springexam.domain.enums.RequestState;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity(name = "request")
+public class Request {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 75, nullable = false)
+	private String subject;
+	
+	@Column(columnDefinition = "text")
+	private String description;
+	
+	@Column(name = "creation_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP) //TemporalType.Date, TemporalType.TIME, TemporalType.TIMESTAMP
+	private Date creationDate;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@OneToMany(mappedBy = "request")
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
 }
+  
